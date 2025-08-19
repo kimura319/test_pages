c36a30f57b9596dd71ba6f7b4a59638e845ae7e0
kimura319 <kimura319@kanzawa-es.com>
2025-08-19 14:32:25 +0900

    コメント削除

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,19 +1,9 @@
-/**
- * @file hello.cpp
- * @brief Hello Worldプログラムの実装
- */
-
 #include <iostream>
 
 #ifndef ADD_NUM
 #define ADD_NUM 5
 #endif
 
-/*!
- * @brief <b>メイン関数</b>
- * @details プログラムのエントリーポイント。"Hello, World!"をコンソールに出力します
- * @retval 0 正常終了
- */
 int main()
 {
     int num = 10;
